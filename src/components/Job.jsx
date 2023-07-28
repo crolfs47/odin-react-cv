c02b15922fdfb8323052aff3d5c8d8a0a5272de6
Cathy Rolfs <crolfs47@gmail.com>
2023-07-28 13:41:15 -0500

    Add edit button functionality to JobsList component

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,18 +1,20 @@
-const Job = ({job}) => {
+
+const Job = ({ job }) => {
 
   return (
-    <div>
-      <div className="flex space-between">
-        <div className="bold">{job.companyName}</div>
+    <>
+      <div>
+        <div className="flex space-between">
+          <div className="bold">{job.companyName}</div>
           <div>
             {job.startDate} - {job.endDate}
           </div>
+        </div>
+        <div className="italic">{job.title}</div>
+        <div>{job.description}</div>
       </div>
-      <div className="italic">{job.title}</div>
-      <div>{job.description}</div>
-    </div>
+    </>
   );
-
-}
+};
 
 export default Job;
--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Job from "./Job";
 
 const JobsList = ({ jobsList }) => {
+  const [isEdit, setEdit] = useState(false);
+
+  const handleEdit = () => {
+    setEdit(!isEdit);
+  };
+
   return (
     <>
-      <div>
-        {jobsList.map((job) => {
-          return <Job key={job.id} job={job} />;
-        })}
-      </div>
+      {!isEdit ? (
+        <div className="section-container">
+          <div className="bottom-border flex space-between">
+            <h2>Experience</h2>
+            <button onClick={handleEdit}>Edit</button>
+          </div>
+
+          <div>
+            {jobsList.map((job) => {
+              return <Job key={job.id} job={job} />;
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="form-container"></div>
+      )}
     </>
   );
 };
